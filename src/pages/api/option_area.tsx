2a564a9b6313ee9aad365ee42225f46449ed4eb9
Farrizal Alchudry Mutaqien <farrizalchudry@gmail.com>
2020-08-02 00:06:53 +0700

    chore(api): add unique filter on option_area api

--- src/pages/api/option_area.tsx
+++ src/pages/api/option_area.tsx
@@ -1,14 +1,28 @@
-import superagent from 'superagent';
-import { Request, Response } from 'express';
-import { API_URI } from '../../config';
-import { AreaModel } from '../../models/area';
+import superagent from "superagent";
+import { Request, Response } from "express";
+import { API_URI } from "../../config";
+import { AreaModel } from "../../models/area";
 
-export default async (_: Request, res: Response) => {
+export default async (req: Request, res: Response) => {
   try {
+    const query = req?.query || {};
     const response = await superagent.get(`${API_URI}/option_area`);
-    const filteredResponse = response?.body?.filter((item: AreaModel) => item.city !== null);
+    let filteredResponse = response?.body?.filter(
+      (item: AreaModel) => item.city !== null
+    );
+    const haveSeen = {};
+    if (query?.filter === "province") {
+      const uniqueResponse = [];
+      filteredResponse.forEach((element) => {
+        if (!haveSeen[element.province]) {
+          uniqueResponse.push({ province: element.province });
+        }
+        haveSeen[element.province] = element.province;
+      });
+      return res.json(uniqueResponse);
+    }
     res.json(filteredResponse);
   } catch (error) {
-    throw Error(error);   
+    throw Error(error);
   }
-}
+};
